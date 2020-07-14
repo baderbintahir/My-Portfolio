72c24a1adb7d7280092c66e3e88b5a6d9224d7f3
baderbintahir <badarbintahir@gmail.com>
2020-07-14 15:52:31 +0500

    #5: made changes in contact methode

--- js/script.js
+++ js/script.js
@@ -6,26 +6,6 @@ spans.forEach(span => span.addEventListener('mouseout', function(e){
     span.classList.remove('animated', 'rubberBand')
 }))
 
-const htmlBar = document.querySelector('.bar-html')
-const cssBar = document.querySelector('.bar-css')
-const javascriptBar = document.querySelector('.bar-javascript')
-const reactBar = document.querySelector('.bar-react')
-
-var t1 = new TimelineLite();
-
-t1.fromTo(htmlBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ease: Power4.easeOut})
-    .fromTo(cssBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(95% - 6px)`, ease: Power4.easeOut})
-    .fromTo(javascriptBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(95% - 6px)`, ease: Power4.easeOut})
-    .fromTo(reactBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(85% - 6px)`, ease: Power4.easeOut})
-
-const controller = new ScrollMagic.Controller()
-const scene = new ScrollMagic.Scene({
-    triggerElement: '.skills',
-    triggerHook: 0
-})
-.setTween(t1)
-.addTo(controller)
-
 
 const showRequiredCategory = event => {
     const getId = event.id
